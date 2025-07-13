bd0117f909a9f9a423a2fec4e43b6f592b43e4f0
aagreet-sinha <aagreetsinha@gmail.com>
2025-07-13 13:22:27 +0530

    Initial commit

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,8 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'eb1fbaf480994c9297f3189198e3a402',
+
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
